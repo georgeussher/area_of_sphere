f775f86b7250cf456213b352eac43a532cbe8396
georgeussher <gussher@icloud.com>
2023-10-23 18:59:02 +0100

    logic created

---
+++ volume_of_sphere.py
@@ -0,0 +1,6 @@
+def vol_of_sphere():
+    radius = int(input("what is the radius in centimetres?"))
+    volume = (4 / 3) * 3 / 1415926 * radius**3
+    print(f"the volume is {volume}cm^3")
+
+vol_of_sphere()
